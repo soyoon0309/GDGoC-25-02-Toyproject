b2db4cefba1d4f124c9b5051c55ae10b92cedb23
soyoon0309 <soyoon0309@naver.com>
2025-11-16 23:16:07 +0900

    Build: oauth 민감정보 보호 파일 형식 변경
    .env 파일 형식에서 .yml 파일 형식으로 저장 방식을 변경함에 따라 SpringBootDeveloperApplication.java의 내용을 간략하게 수정하고 해당 코드를 .gitignore에 반영함

--- src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -4,17 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import io.github.cdimascio.dotenv.Dotenv;
-@EnableJpaAuditing //create_at, updated_at 자동 업데이트
+@EnableJpaAuditing  //created_at, updated_at auto update
 @SpringBootApplication
-
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
-
-        // .env 파일 로드
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
-        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
 }
